01cb533d464a8715667b92bed092285b6985f2a6
mirandars2002 <mirandars1234@gmail.com>
2024-01-22 16:17:04 +0100

    he subdio

--- TranajoInglesV2/src/src/app/gui/cesta/cestaController.java
+++ TranajoInglesV2/src/src/app/gui/cesta/cestaController.java
@@ -1,28 +1,16 @@
 package src.app.gui.cesta;
 
 import javafx.fxml.FXML;
-import javafx.geometry.Insets;
-import javafx.scene.Scene;
+
 import javafx.scene.control.Button;
-import javafx.scene.control.Label;
-import javafx.scene.image.Image;
-import javafx.scene.image.ImageView;
-import javafx.scene.layout.HBox;
-import javafx.scene.layout.VBox;
-import modelo.Producto;
+
 import src.app.gui.AppController;
 import src.app.gui.FXMLPaths;
-import java.math.BigDecimal;
 
 public class cestaController extends AppController {
 
 	@FXML
 	private Button boton;
-
-	
-
-
-	
 
 	@FXML
 	public void compra() {
