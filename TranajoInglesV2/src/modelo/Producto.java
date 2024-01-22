97219d797133ebb51ed30097d5217cd0597f1bf8
mirandars2002 <mirandars1234@gmail.com>
2024-01-22 15:41:06 +0100

    kositas nuebas

--- TranajoInglesV2/src/modelo/Producto.java
+++ TranajoInglesV2/src/modelo/Producto.java
@@ -12,39 +12,54 @@ public class Producto {
 	private BigDecimal precio;
 	private String descripcion;
 	private Integer idUsuario;
+
+	public Producto(String nombre, BigDecimal precio) {
+		this.nombre = nombre;
+		this.precio = precio;
+	}
 	
+	public Producto() {
+		
+	}
+
 	public Integer getIdProducto() {
 		return idProducto;
 	}
+
 	public void setIdProducto(Integer idProducto) {
 		this.idProducto = idProducto;
 	}
+
 	public String getNombre() {
 		return nombre;
 	}
+
 	public void setNombre(String nombre) {
 		this.nombre = nombre;
 	}
+
 	public BigDecimal getPrecio() {
 		return precio;
 	}
+
 	public void setPrecio(BigDecimal precio) {
 		this.precio = precio;
 	}
+
 	public String getDescripcion() {
 		return descripcion;
 	}
+
 	public void setDescripcion(String descripcion) {
 		this.descripcion = descripcion;
 	}
+
 	public Integer getIdUsuario() {
 		return idUsuario;
 	}
+
 	public void setIdUsuario(Integer idUsuario) {
 		this.idUsuario = idUsuario;
 	}
-	
-	
-	
-	
+
 }
