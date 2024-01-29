5ef764f9a91d479d3e734627299983513397d511
pablo <paretosfas@gmail.com>
2024-01-29 16:14:12 +0100

    subido todo

--- TranajoInglesV2/src/src/app/gui/registrar/registrarController.java
+++ TranajoInglesV2/src/src/app/gui/registrar/registrarController.java
@@ -1,9 +1,15 @@
 package src.app.gui.registrar;
 
+import java.security.NoSuchAlgorithmException;
+import java.sql.SQLException;
+
 import javafx.fxml.FXML;
-
+import javafx.scene.control.Alert;
+import javafx.scene.control.Alert.AlertType;
 import javafx.scene.control.Button;
-
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+import modelo.Usuario;
 import src.app.gui.AppController;
 import src.app.gui.FXMLPaths;
 
@@ -11,15 +17,48 @@ public class registrarController extends AppController {
 
 	@FXML
 	private Button boton;
+	@FXML
+	private TextField tfNombre;
+	@FXML
+	private TextField tfUser;
+	@FXML
+	private PasswordField tfPass;
 
 	@FXML
 	public void entrar() {
 
-		cambiarVista(FXMLPaths.COMPRA);
+		Boolean correcto = false;
+		if (tfNombre != null && tfUser != null && tfPass != null) {
+			Usuario us = new Usuario();
+			us.setNombre(tfNombre.getText());
+			us.setUsuario(tfUser.getText());
+			us.setContraseña(tfPass.getText());
+			try {
+				correcto = usuSer.registrar(us);
+			} catch (NoSuchAlgorithmException e) {
+				e.printStackTrace();
+			} catch (SQLException e) {
+				e.printStackTrace();
+			}
+			if (correcto) {
+				cambiarVista(FXMLPaths.INICIAR);
+			} else {
+				Alert a = new Alert(AlertType.ERROR);
+				a.setHeaderText(null);
+				a.setContentText("Error en el usuario o contraseña");
+				a.setTitle("Error");
+				a.showAndWait();
+			}
+		} else {
+			Alert a = new Alert(AlertType.ERROR);
+			a.setHeaderText(null);
+			a.setContentText("Error en el usuario o contraseña");
+			a.setTitle("Error");
+			a.showAndWait();
+		}
 
 	}
-	
-	
+
 	@FXML
 	public void home() {
 
